717bffab57ec5817aaffdef7b17e134deb31c399
Dave Baker <david@fullybaked.co.uk>
2022-09-23 21:51:50 +0100

    Add firebase (#14)

--- src/api/config.js
+++ src/api/config.js
@@ -3,13 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'FILL_ME_IN',
-	authDomain: 'FILL_ME_IN',
-	databaseURL: 'FILL_ME_IN',
-	projectId: 'FILL_ME_IN',
-	storageBucket: 'FILL_ME_IN',
-	messagingSenderId: 'FILL_ME_IN',
-	appId: 'FILL_ME_IN',
+  apiKey: "AIzaSyAciw2fWDwjzLLo1AyhMADkONNQAwen-3o",
+  authDomain: "tcl-49-smart-shopping-list.firebaseapp.com",
+  projectId: "tcl-49-smart-shopping-list",
+  storageBucket: "tcl-49-smart-shopping-list.appspot.com",
+  messagingSenderId: "618274190288",
+  appId: "1:618274190288:web:ff9c8967a750fdc24d3fd5"
 };
 
 // Initialize Firebase
