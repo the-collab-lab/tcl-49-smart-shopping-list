6000e0c68fa7b81804bcbae1af851d9f1965042b
teri eyenike <teyenike1@gmail.com>
2022-10-04 13:20:28 +0100

    Update: loop through the data lists and destructure the array

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ListItem } from '../components';
 
 export function List({ data }) {
@@ -7,9 +8,10 @@ export function List({ data }) {
 				Hello from the <code>/list</code> page!
 			</p>
 			<ul>
-				{data.map((item) => (
-					<ListItem key={item.id} name={item.name} />
-				))}
+				{React.Children.toArray(
+					data.map(({ name }) => <ListItem name={name} />),
+				)}
+
 				{/**
 				 * TODO: write some JavaScript that renders the `data` array
 				 * using the `ListItem` component that's imported at the top
