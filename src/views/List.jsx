738db4e5cf6942deecc773ce8af3b76776a00a9d
teri eyenike <teyenike1@gmail.com>
2022-10-04 10:43:48 +0100

    Add: render list component

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,12 +1,17 @@
+// import React from 'react'
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	console.log(data);
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
 			<ul>
+				{data.map((item) => (
+					<ListItem key={item.id} name={item.name} />
+				))}
 				{/**
 				 * TODO: write some JavaScript that renders the `data` array
 				 * using the `ListItem` component that's imported at the top
