72c56e4d0dc06b49bd3eb92c43668954d43005c5
teri eyenike <teyenike1@gmail.com>
2022-10-10 10:50:02 +0100

    Add: create a form for the add items

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,3 +1,19 @@
 export function AddItem() {
-	return <p>Hello from the <code>/add-item</code> page!</p>
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('it is working!!!');
+	};
+	return (
+		<div>
+			<h1>Smart shopping list</h1>
+
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="items">Item name</label>
+					<input type="text" />
+				</div>
+				<button type="submit">add item</button>
+			</form>
+		</div>
+	);
 }
