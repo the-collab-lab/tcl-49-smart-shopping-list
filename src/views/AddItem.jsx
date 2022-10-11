2fa95fdd0b8ac449bdb3915e77598e3095001921
mohanner-coder <victoria.okafor.ng@gmail.com>
2022-10-11 12:36:20 +0100

    Added radio bottons and react hooks

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,18 +1,64 @@
+import { useState } from 'react';
+
 export function AddItem() {
+	const [value, setValue] = useState(false);
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		console.log('it is working!!!');
 	};
-	return (
-		<div>
-			<h1>Smart shopping list</h1>
 
+	const handleChange = () => {
+		setValue(!value);
+	};
+
+	return (
+		<div className="shopping-list">
 			<form onSubmit={handleSubmit}>
 				<div>
-					<label htmlFor="items">Item name</label>
-					<input type="text" />
+					<h4>Item name:</h4>
 				</div>
-				<button type="submit">add item</button>
+
+				<div className="search__input">
+					{' '}
+					<input className="input" type="text" placeholder=" Eggs" />{' '}
+				</div>
+				<p>How soon will you buy this again?</p>
+
+				<label>
+					<input type="radio" value={'soon'} onChange={handleChange} />
+					soon
+				</label>
+				<br />
+				<br />
+				<label>
+					<input type="radio" value={'Kind of soon'} onChange={handleChange} />
+					Kind of soon
+				</label>
+				<br />
+				<br />
+
+				<label>
+					<input type="radio" value={'not soon'} onChange={handleChange} />
+					Not soon
+				</label>
+				<br />
+				<br />
+				<br />
+				<br />
+
+				<div>
+					{' '}
+					<button type="submit">List</button>
+				</div>
+				<br />
+				<br />
+				<br />
+				<br />
+				<div>
+					<button type="submit">List</button>
+					<button type="submit">Add item</button>
+				</div>
 			</form>
 		</div>
 	);
