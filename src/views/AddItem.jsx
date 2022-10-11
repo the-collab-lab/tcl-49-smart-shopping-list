c1491e6003d58c9079058aadda76b0df335ed4bb
teri eyenike <teyenike1@gmail.com>
2022-10-11 15:08:07 +0100

    Add: slight change to the add item component

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -12,6 +12,9 @@ export function AddItem() {
 					<label htmlFor="items">Item name</label>
 					<input type="text" />
 				</div>
+
+				<h3>how soon will you buy this again?</h3>
+				<div></div>
 				<button type="submit">add item</button>
 			</form>
 		</div>
