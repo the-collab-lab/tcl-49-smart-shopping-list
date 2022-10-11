b1ea191f357fec57c5cb2e5629ee0f071a287a47
Cristina Padilla <crispitipina@gmail.com>
2022-10-11 19:31:51 +0200

    Add function assumptions

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,6 +5,8 @@ export function Home() {
 	function handleClick() {
 		const token = generateToken();
 		console.log(token);
+		//if user doesn't have a token, generates new token, saves and creates new list
+		//if user has a token, redirects to List
 	}
 
 	return (
