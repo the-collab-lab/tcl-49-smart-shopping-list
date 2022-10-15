e0a7e3e3028c1dc4c691e48dfac46d69f923253c
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-10-15 23:00:49 +0400

    Removed unnecessary comments

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import './index.css';
 
-// const root = createRoot(document.getElementById('root'));
-// root.render(<App />);
-
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
