ef1c54fa6c5ea869d267879c38ada138f80e7558
mohanner-coder <victoria.okafor.ng@gmail.com>
2022-10-17 11:50:31 +0100

    Adds token form and submit button

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,37 @@
 import './Home.css';
+import { FormInput } from './../components/form-input/form-input.component';
+import { useState } from 'react';
+import { Button } from './../components/button/button.component';
 
 export function Home() {
+	const [displayName, setDisplayName] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		console.log('Submitting..');
+	};
+
+	const handleChange = (e) => {
+		const { value } = e.target;
+		setDisplayName(value);
+	};
+
 	return (
 		<div className="Home">
 			<p>
 				Hello from the home (<code>/</code>) page!
 			</p>
+			<form onSubmit={handleSubmit}>
+				<FormInput
+					label="Enter Token"
+					type="text"
+					onChange={handleChange}
+					name="displayName"
+					id="token"
+					value={displayName}
+				/>
+				<Button type="submit">Submit</Button>
+			</form>
 		</div>
 	);
 }
