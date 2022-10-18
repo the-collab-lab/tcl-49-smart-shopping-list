28b83ec32f32125fcd7a731c7aecd4b685770d91
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-10-18 20:16:46 +0400

    Added a form to List page

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,5 +1,17 @@
 import './ListItem.css';
+import React from 'react';
 
 export function ListItem({ name }) {
-	return <li className="ListItem">{name}</li>;
+	return (
+		<React.Fragment>
+			<div>
+				<input type="checkbox" />
+				<li className="ListItem">{name}</li>
+			</div>
+			<div>
+				<button className="">Details</button>
+				<button className="">Delete</button>
+			</div>
+		</React.Fragment>
+	);
 }
--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,11 +1,26 @@
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	const handleFilterItems = (event) => {
+		event.preventDefault();
+		console.log('This works!');
+	};
+
 	return (
 		<>
-			<p>
-				Hello from the <code>/list</code> page!
-			</p>
+			{/* Add a form */}
+			<div>
+				<form onSubmit={handleFilterItems}>
+					<label htmlFor="filter-items">Filter items</label>
+					<input
+						type="text"
+						name="filter-items"
+						id="filter-items"
+						placeholder="Start typing here..."
+					/>
+				</form>
+			</div>
+
 			<ul>
 				{data.map(({ name, id }) => (
 					<ListItem key={id} name={name} />
