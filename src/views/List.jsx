d58a66fe011f1f7bf2322c6c26703877d270bc14
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-10-20 01:30:35 +0400

    Added some deletion handlers to handle dynamic removal

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -3,7 +3,36 @@ import { ListItem } from '../components';
 
 export function List({ data }) {
 	const [searchField, setSearchField] = useState('');
-	const [buttonPopUp, setButtonPopUp] = useState(false);
+	const [buttonPopUp, setButtonPopUp] = useState({
+		show: false,
+		id: null,
+	});
+
+	const handleDelete = (id) => {
+		setButtonPopUp({
+			show: true,
+			id,
+		});
+	};
+
+	const handleDeleteTrue = () => {
+		if (buttonPopUp.show && buttonPopUp.id) {
+			data.filter((item) => item.id !== buttonPopUp.id);
+			setButtonPopUp({
+				show: false,
+				id: null,
+			});
+		}
+	};
+
+	// This will just hide the Confirmation Box when user clicks "No"/"Cancel"
+
+	const handleDeleteFalse = () => {
+		setButtonPopUp({
+			show: false,
+			id: null,
+		});
+	};
 
 	const onSearchChange = (e) => {
 		const { value } = e.target;
