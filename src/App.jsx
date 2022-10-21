bc1af41dd251640eb8ff8932fd92bc32bf0d00df
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-10-21 22:14:32 +0400

    Removed all unnecessary comments

--- src/App.jsx
+++ src/App.jsx
@@ -26,15 +26,8 @@ export function App() {
 			navigate('/list');
 
 			return streamListItems(listToken, (snapshot) => {
-				// 		/**
-				// 		 * Read the documents in the snapshot and do some work
-				// 		 * on them, so we can save them in our React state.
-				// 		 *
-				// 		 * Refer to `api/firebase.js`
-				// 		 */
 				const nextData = getItemData(snapshot);
 
-				// 		/** Finally, we update our React state. */
 				setData(nextData);
 			});
 		}
