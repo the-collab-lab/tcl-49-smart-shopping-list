178f03d48ad1a6a7d3bf4b1c8870a03391531bc1
teri eyenike <teyenike1@gmail.com>
2022-10-27 23:41:01 +0100

    Update: try different methodfor the time

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,29 +1,11 @@
 import './ListItem.css';
 import { updateItem } from '../api';
 
-const currentDate = new Date();
-const currentTime = currentDate.getTime() / 1000; //gets date in milliseconds since Jan 1, 1970 and converts to seconds
-
 export function ListItem({ name, items, listToken }) {
 	const { isChecked, id, totalPurchases, dateLastPurchased } = items;
-	// const currentDate = new Date();
+
+	const currentDate = new Date();
 	const updateTotalPurchases = totalPurchases + 1;
-
-	// console.log({ dateLastPurchased });
-
-	if (isChecked) {
-		const lastPurchaseToSec = dateLastPurchased / 1000;
-
-		const differenceInMinutes = (currentTime - lastPurchaseToSec) / 60;
-
-		const timeToUncheck = 120; //minutes to uncheck automatically: 24 hours x 60 min = 1440
-		if (differenceInMinutes >= timeToUncheck) {
-			const itemData = {
-				isChecked: false,
-			};
-			updateItem(listToken, id, itemData);
-		}
-	}
 
 	const handleCheckbox = () => {
 		if (isChecked) {
@@ -43,15 +25,18 @@ export function ListItem({ name, items, listToken }) {
 	};
 
 	return (
-		<li className="ListItem">
-			<input
-				type="checkbox"
-				name="purchased"
-				id="purchased"
-				onChange={handleCheckbox}
-				defaultChecked={isChecked}
-			/>
-			<label htmlFor="purchased">{name}</label>
-		</li>
+		<>
+			<li className="ListItem">
+				<input
+					type="checkbox"
+					name="purchased"
+					id="purchased"
+					onChange={handleCheckbox}
+					defaultChecked={isChecked}
+				/>
+				<label htmlFor="purchased">{name}</label>
+			</li>
+			<p>Total purchases: {totalPurchases}</p>
+		</>
 	);
 }
