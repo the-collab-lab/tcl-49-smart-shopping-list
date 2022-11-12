e5444de755302eda83ede34e5a6271146843ce7e
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-11-12 16:58:25 +0400

    Yet another fix

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -16,116 +16,113 @@ export function AddItem({ listToken, itemList }) {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
-			const isEmpty = () => itemName.trim().length === 0;
+		const isEmpty = () => itemName.trim().length === 0;
 
-			const isDuplicated = () => {
-				const itemNames = itemList.map((item) =>
-					item.name.toLowerCase().replace(/[^a-zA-Z0-9]+/g, ''),
-				);
-				return itemNames.includes(
-					itemName.toLowerCase().replace(/[^a-zA-Z0-9]+/g, ''),
-				);
-			};
-
-			try {
-				if (isEmpty()) {
-					setMessage("Error: Please enter your item's name.");
-				} else if (isDuplicated()) {
-					setMessage(
-						`${itemName} is already on your list. Please add a different item.`,
-					);
-				} else {
-					await addItem(listToken, {
-						itemName,
-						daysUntilNextPurchase,
-					});
-					setMessage(
-						`${itemName} was successfully saved to your shopping list!`,
-					);
-				}
-			} catch (error) {
-				setMessage('Error: This item was not added to the database.');
-			}
+		const isDuplicated = () => {
+			const itemNames = itemList.map((item) =>
+				item.name.toLowerCase().replace(/[^a-zA-Z0-9]+/g, ''),
+			);
+			return itemNames.includes(
+				itemName.toLowerCase().replace(/[^a-zA-Z0-9]+/g, ''),
+			);
 		};
 
-		const handleChange = (e) => {
-			if (message) {
-				setMessage('');
+		try {
+			if (isEmpty()) {
+				setMessage("Error: Please enter your item's name.");
+			} else if (isDuplicated()) {
+				setMessage(
+					`${itemName} is already on your list. Please add a different item.`,
+				);
+			} else {
+				await addItem(listToken, {
+					itemName,
+					daysUntilNextPurchase,
+				});
+				setMessage(`${itemName} was successfully saved to your shopping list!`);
 			}
-			setFormFields((prevState) => ({
-				...prevState,
-				[e.target.name]: e.target.value,
-			}));
-		};
-
-		return (
-			<div className="shopping-form-container">
-				{message ? (
-					<div>
-						<h2>{message}</h2>
-						<Link to="/list">
-							<button>View the list</button>
-						</Link>
-					</div>
-				) : (
-					<form onSubmit={handleSubmit}>
-						<FormInput
-							label="Item name"
-							type="text"
-							onChange={handleChange}
-							name="itemName"
-							id="itemName"
-							required
-							placeholder="item name"
-							value={itemName}
-						/>
-						<div className="select-next-purchase-buttons">
-							<fieldset>
-								<legend>How soon will you buy this again?</legend>
-								<div className="radio-btn">
-									<label htmlFor="soon">
-										<input
-											type="radio"
-											name="daysUntilNextPurchase"
-											id="soon"
-											value={7 || daysUntilNextPurchase}
-											onChange={handleChange}
-											defaultChecked
-										/>
-										Soon
-									</label>
-								</div>
-								<div className="radio-btn">
-									<label htmlFor="notSoon">
-										<input
-											type="radio"
-											name="daysUntilNextPurchase"
-											id="notSoon"
-											value={14 || daysUntilNextPurchase}
-											onChange={handleChange}
-										/>
-										Not Soon
-									</label>
-								</div>
-								<div className="radio-btn">
-									<label htmlFor="kindOfSoon">
-										<input
-											type="radio"
-											name="daysUntilNextPurchase"
-											id="kindOfSoon"
-											value={30 || daysUntilNextPurchase}
-											onChange={handleChange}
-										/>
-										Kind of Soon
-									</label>
-								</div>
-							</fieldset>
-						</div>
-
-						<Button type="submit">Add Item</Button>
-					</form>
-				)}
-			</div>
-		);
+		} catch (error) {
+			setMessage('Error: This item was not added to the database.');
+		}
 	};
+
+	const handleChange = (e) => {
+		if (message) {
+			setMessage('');
+		}
+		setFormFields((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	return (
+		<div className="shopping-form-container">
+			{message ? (
+				<div>
+					<h2>{message}</h2>
+					<Link to="/list">
+						<button>View the list</button>
+					</Link>
+				</div>
+			) : (
+				<form onSubmit={handleSubmit}>
+					<FormInput
+						label="Item name"
+						type="text"
+						onChange={handleChange}
+						name="itemName"
+						id="itemName"
+						required
+						placeholder="item name"
+						value={itemName}
+					/>
+					<div className="select-next-purchase-buttons">
+						<fieldset>
+							<legend>How soon will you buy this again?</legend>
+							<div className="radio-btn">
+								<label htmlFor="soon">
+									<input
+										type="radio"
+										name="daysUntilNextPurchase"
+										id="soon"
+										value={7 || daysUntilNextPurchase}
+										onChange={handleChange}
+										defaultChecked
+									/>
+									Soon
+								</label>
+							</div>
+							<div className="radio-btn">
+								<label htmlFor="notSoon">
+									<input
+										type="radio"
+										name="daysUntilNextPurchase"
+										id="notSoon"
+										value={14 || daysUntilNextPurchase}
+										onChange={handleChange}
+									/>
+									Not Soon
+								</label>
+							</div>
+							<div className="radio-btn">
+								<label htmlFor="kindOfSoon">
+									<input
+										type="radio"
+										name="daysUntilNextPurchase"
+										id="kindOfSoon"
+										value={30 || daysUntilNextPurchase}
+										onChange={handleChange}
+									/>
+									Kind of Soon
+								</label>
+							</div>
+						</fieldset>
+					</div>
+
+					<Button type="submit">Add Item</Button>
+				</form>
+			)}
+		</div>
+	);
 }
