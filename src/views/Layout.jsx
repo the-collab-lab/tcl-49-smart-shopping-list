a56bb1ef8a0a54ff5dad8c609bb43ebb36e89c9e
Cristina Padilla <68116083+Mama-simba@users.noreply.github.com>
2022-11-29 13:24:30 +0100

    install hero icons and add icons

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,4 +1,8 @@
 import { Outlet, NavLink } from 'react-router-dom';
+import { ShoppingCartIcon } from '@heroicons/react/24/solid';
+import { HomeIcon } from '@heroicons/react/24/solid';
+import { QueueListIcon } from '@heroicons/react/24/solid';
+import { PlusCircleIcon } from '@heroicons/react/24/solid';
 
 import './Layout.css';
 
@@ -6,8 +10,9 @@ export function Layout() {
 	return (
 		<>
 			<div className="Layout mt-14 w-4/5 mx-auto">
-				<header className="Layout-header">
-					<h1 className="text-4xl mb-4 text-center font-bold underline text-white ">
+				<header className="Layout-header flex justify-center items-stretch">
+					<ShoppingCartIcon className="h-11 w-11 text-white mx-1 drop-shadow" />
+					<h1 className="text-4xl mb-4 text-center font-bold underline text-white">
 						Smart shopping list
 					</h1>
 				</header>
@@ -17,20 +22,23 @@ export function Layout() {
 				<nav className="Nav bg-white rounded-lg drop-shadow-lg mt-8 py-4 flex justify-around">
 					<NavLink
 						to="/"
-						className="Nav-link font-semibold hover:text-green-600 focus:text-green-600"
+						className="Nav-link flex items-center font-semibold hover:text-green-600 focus:text-green-600"
 					>
+						<HomeIcon className="h-4 w-4" />
 						Home
 					</NavLink>
 					<NavLink
 						to="/list"
-						className="Nav-link font-semibold hover:text-green-600 focus:text-green-600"
+						className="Nav-link flex items-center font-semibold hover:text-green-600 focus:text-green-600"
 					>
+						<QueueListIcon className="h-4 w-4" />
 						List
 					</NavLink>
 					<NavLink
 						to="/add-item"
-						className="Nav-link font-semibold hover:text-green-600 focus:text-green-600"
+						className="Nav-link flex items-center font-semibold hover:text-green-600 focus:text-green-600"
 					>
+						<PlusCircleIcon className="h-4 w-4" />
 						Add Item
 					</NavLink>
 				</nav>
