4559a810bfd3e6bd4d4bf7bb0cee94f06e59801e
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-12-01 23:36:40 +0400

    Cleaned up the homepage and added color to the error

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -25,7 +25,7 @@ export function Home({
 			setListToken(displayName, 'tcl-shopping-list-token');
 			navigate('/list');
 		} else {
-			setErrorMessage('Invalid token | The list does not exist');
+			setErrorMessage('Error! Invalid token: this list does not exist. :(');
 		}
 
 		setDisplayName('');
@@ -35,7 +35,7 @@ export function Home({
 		<div className="main-layout Home w-full mx-auto py-9 bg-white rounded-lg p-10 text-center">
 			{!listToken && (
 				<button
-					className="bg-green-600 py-2 px-3 rounded-full text-white shadow-md hover:text-green-600 hover:bg-transparent hover:border-solid hover:border-2 hover:font-semibold"
+					className="bg-green-500 py-1 px-3 mb-4 rounded text-white hover:text-green-700 hover:bg-transparent hover:border-2 hover:font-normal"
 					onClick={handleClick}
 				>
 					Create list
@@ -52,8 +52,15 @@ export function Home({
 					value={displayName}
 					placeholder="my test token"
 				/>
-				<p>{errorMessage}</p>
-				<Button type="submit">Join an existing list</Button>
+				<p className="text-red-600 m-4" role="alert">
+					{errorMessage}
+				</p>
+				<button
+					type="submit"
+					className="bg-green-500 py-1 px-3 mt-4 mb-2 rounded text-white hover:text-green-700 hover:bg-transparent hover:border-2 hover:font-normal"
+				>
+					Join an existing list
+				</button>
 			</form>
 		</div>
 	);
