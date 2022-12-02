871826555756dd275bf6bb0db5f935e2b6feca22
Cristina Padilla <68116083+Mama-simba@users.noreply.github.com>
2022-12-02 11:21:21 +0100

    change error message color

--- src/components/ListPrompt.jsx
+++ src/components/ListPrompt.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 export default function PromptButton() {
 	return (
 		<div className="w-full flex flex-col items-center">
-			<p className="text-center text-red-700 m-2 font-normal">
-				Your shopping list is currently empty. Please add an item. :)
+			<p className="text-center text-yellow-200 m-2 font-normal">
+				Your shopping list is currently empty. Please add an item 🙂
 			</p>
 			<Link
 				className="bg-green-500 py-1 px-3 m-6 rounded text-white hover:text-green-700 hover:bg-transparent hover:border-2 hover:font-normal"
